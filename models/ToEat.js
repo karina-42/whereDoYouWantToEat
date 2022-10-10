abe5ec7d33ac48b64796a236af78eb147b295506
Angela Vegega <angela.vegega@gmail.com>
2022-10-10 11:48:08 +0900

    Added form and displayed reminders of where to eat next

---
+++ models/ToEat.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const ToEatSchema = new mongoose.Schema({
+  toEat: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model("ToEat", ToEatSchema)
--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector('#random-food')
+const deleteBtn = document.querySelectorAll('.del')
 
+Array.from(deleteBtn).forEach((el)=>{
+  el.addEventListener('click', deleteToEat)
+})
 
 btn.addEventListener('click', function(e) {
   fetch('/randomFood', {method: 'GET'})
@@ -23,3 +27,21 @@ btn.addEventListener('click', function(e) {
     eat.textContent = `How about ${pickRandomFood}?`
   })
 })
+
+async function deleteToEat(){
+  const toEatId = this.parentNode.dataset.id
+  try{
+      const response = await fetch('/deleteToEat', {
+          method: 'delete',
+          headers: {'Content-type': 'application/json'},
+          body: JSON.stringify({
+              'toEatIdFromJSFile': toEatId
+          })
+      })
+      const data = await response.json()
+      console.log(data)
+      location.reload()
+  }catch(err){
+      console.log(err)
+  }
+}
